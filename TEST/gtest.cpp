ec7fb0623809045539d1935a781f88e3dc30617f
liuhubing <hubing.liu@thoughtworks.com>
2020-05-19 14:28:49 +0800

    first commit

---
+++ TEST/gtest.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "add.h"
+
+TEST(AddTest, HandlerTrueReturn)
+{
+	ASSERT_TRUE(add(1, 2) == 3);
+}
+
+int main(int argc, char*argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
